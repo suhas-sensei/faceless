b349ff752006cc50192c001a586e63f6f39910b4
Suhas Ghosal <suhas.ghosal2002@gmail.com>
2024-09-13 11:59:22 +0530

    typo fixes

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -4,8 +4,8 @@ export default function payment() {
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500">
       <h1 className="text-6xl md:text-8xl font-bold text-white shadow-lg transform transition-transform duration-500 hover:scale-110">
-        paisa de
-        <br></br>lavde
+        payment via
+        <br></br>Phonepe billdesk
       </h1>
     </div>
   );
